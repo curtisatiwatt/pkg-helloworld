c5132d79da5fd23c33ed4b49bc194ab527a1b747
Curtis Atiwatt <curtis.atiwatt@gmail.com>
2024-06-04 14:00:43 -0600

    Add a working copy for pkg with Axios

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hello, World!");
+const axios = require('axios');
+
+axios.get('https://api.github.com/repos/nodejs/node')
+  .then(response => {
+    console.log(`Repository: ${response.data.full_name}`);
+    console.log(`Description: ${response.data.description}`);
+    console.log(`Stars: ${response.data.stargazers_count}`);
+    console.log(`Forks: ${response.data.forks_count}`);
+  })
+  .catch(error => {
+    console.error(`Error fetching data: ${error}`);
+  });
